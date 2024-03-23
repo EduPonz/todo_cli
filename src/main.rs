13131cc28b44f52d6d63a502b40492f22e5f1d22
EduPonz <eduardoponz@eprosima.com>
2024-03-23 08:50:39 +0100

    Add entry level help function
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn print_help() {
+    println!("Usage: todo [-h|--help] [-v|--version] <verb> [options]");
+    println!("");
+    println!("Options:");
+    println!("  -h, --help    Print this help message");
+    println!("  -v, --version Print version information");
+    println!("");
+    println!("todo verbs:");
+    println!("  add           Add a new todo item");
+    println!("  list          List all todo items");
+    println!("  update        Update a todo item");
+    println!("  remove        Remove a todo item");
+    println!("");
+    println!("Run 'todo <verb> --help' for more information on a verb.");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_help();
 }
