5a0db6da64ac02182949e2e57dc654a0001accd1
EduPonz <eduardoponz@eprosima.com>
2024-03-23 09:03:58 +0100

    Add first level option parser
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
 fn print_help() {
-    println!("Usage: todo [-h|--help] [-v|--version] <verb> [options]");
+    println!("Usage: {} [options] <verb> [verb_options]", env!("CARGO_PKG_NAME"));
     println!("");
     println!("Options:");
     println!("  -h, --help    Print this help message");
     println!("  -v, --version Print version information");
     println!("");
-    println!("todo verbs:");
+    println!("{} verbs:", env!("CARGO_PKG_NAME"));
     println!("  add           Add a new todo item");
     println!("  list          List all todo items");
     println!("  update        Update a todo item");
     println!("  remove        Remove a todo item");
     println!("");
-    println!("Run 'todo <verb> --help' for more information on a verb.");
+    println!("Run '{} <verb> --help' for more information on a verb.", env!("CARGO_PKG_NAME"));
+}
+
+fn print_version() {
+    println!("{} version {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+}
+
+fn parse_arguments(args: Vec<String>) {
+    // Check if there are enough arguments
+    if args.len() < 2 {
+        print_help();
+        std::process::exit(1);
+    }
+
+    // Check the first argument
+    match args[1].as_str() {
+        "-h" | "--help" => print_help(),
+        "-v" | "--version" => print_version(),
+        "add" => println!("add"),
+        "list" => println!("list"),
+        "update" => println!("update"),
+        "remove" => println!("remove"),
+        _ => {
+            println!("Unknown verb '{}'", args[1]);
+            print_help();
+            std::process::exit(1);
+        }
+    }
 }
 
 fn main() {
-    print_help();
+    let args: Vec<String> = std::env::args().collect();
+    parse_arguments(args);
 }
