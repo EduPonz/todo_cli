42dfd761aaf9b9de1fed83c6bf0a41e9c2817924
eduponz <eduardoponz@eprosima.com>
2024-04-06 08:41:20 +0200

    Refactor add to use a CSV file
    Signed-off-by: eduponz <eduardoponz@eprosima.com>

--- src/app/add.rs
+++ src/app/add.rs
@@ -1,5 +1,8 @@
 use std::fs::OpenOptions;
-use std::io::prelude::*;
+use std::io::Seek;
+use std::time::SystemTime;
+
+use serde::Serialize;
 
 use crate::app::parser::Parser;
 
@@ -17,34 +20,103 @@ impl Parser for Adder {
     }
 
     fn parse(args: Vec<String>) -> Result<(), String> {
+        let mut ret = Ok(());
+
         // Check if there are enough arguments
         if args.len() == 0 {
             Self::print_help();
-            std::process::exit(1);
+            ret = Err("Not enough arguments".to_string());
         }
 
-        // Check add input
-        match args[0].as_str() {
-            "-h" | "--help" => Self::print_help(),
-            _ => Self::add(args[0].as_str().to_string()),
+        if !ret.is_err() {
+            // Check add input
+            match args[0].as_str() {
+                "-h" | "--help" => {
+                    Self::print_help();
+                    ret = Ok(());
+                }
+                _ => {
+                    ret = CSVAdder::add(args[0].as_str().to_string());
+                }
+            }
         }
 
-        Ok(())
+        ret
     }
 }
 
-impl Adder {
-    fn add(description: String) -> () {
-        println!("Added todo item with description '{}'", description);
+struct CSVAdder();
 
-        let mut file = OpenOptions::new()
+#[derive(Serialize)]
+struct Record<'a> {
+    id: &'a str,
+    description: &'a str,
+    status: &'a str,
+    last_updated: &'a str,
+}
+
+impl CSVAdder {
+    fn add(description: String) -> Result<(), String> {
+        let mut ret = Ok(());
+        println!("Adding todo item with description '{}'", description);
+
+        let mut csv_file = match OpenOptions::new()
+            .write(true)
             .create(true)
             .append(true)
-            .open("todo.txt")
-            .unwrap();
+            .open("todo.csv")
+        {
+            Ok(f) => f,
+            Err(e) => {
+                ret = Err(e.to_string());
+                return ret;
+            }
+        };
 
-        if let Err(e) = writeln!(file, "{}", description) {
-            eprintln!("Couldn't write to file: {}", e);
+        // Headers are needed only if the file is empty
+        let needs_headers = match csv_file.seek(std::io::SeekFrom::End(0)) {
+            Ok(0) => true,
+            Ok(_) => false,
+            Err(e) => {
+                ret = Err(e.to_string());
+                return ret;
+            }
+        };
+
+        // Create a CSV writer
+        let mut csv_writer = csv::WriterBuilder::new()
+            .has_headers(needs_headers)
+            .from_writer(csv_file);
+
+        if !ret.is_err() {
+            let timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs()
+                .to_string();
+
+            let record = Record {
+                id: &timestamp,
+                status: "Pending",
+                description: &description,
+                last_updated: &timestamp,
+            };
+
+            match csv_writer.serialize(record) {
+                Ok(_) => (),
+                Err(_e) => {
+                    ret = Err("Failed to write to CSV file 'todo.csv'".to_string());
+                }
+            };
+
+            match csv_writer.flush() {
+                Ok(_) => (),
+                Err(_e) => {
+                    ret = Err("Failed to flush CSV file 'todo.csv'".to_string());
+                }
+            };
         }
+
+        ret
     }
 }
